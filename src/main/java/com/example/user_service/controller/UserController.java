38945008ac9dc109a27d00380515e25973bb1ed5
Oskar Picus <oskarpicus@yahoo.com>
2023-12-17 10:19:34 +0200

    Added CrossOrigin

--- src/main/java/com/example/user_service/controller/UserController.java
+++ src/main/java/com/example/user_service/controller/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import com.example.user_service.service.UserService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin
 public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
